a8c76ac42a80a19d09b62242f493bc29f075d63f
Samar <s99502042@gmail.com>
2021-11-08 17:31:23 +0800

    spinner

---
+++ my-app/src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../pages/st/style/st_course.css'
+export default function Spinner() {
+  return (
+    <>
+      <div className="loadingio-spinner-ripple-axvmfinlmje ">
+        <div className="ldio-xv8fxyexxut">
+          <div> </div> <div> </div>
+        </div>
+      </div>
+    </>
+  )
+}
