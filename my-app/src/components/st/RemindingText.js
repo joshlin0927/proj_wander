beed3cb53a83f8cac0d0bd8b09bd53abc09b37ed
NetherCod <six0860613@gmail.com>
2021-11-12 16:39:22 +0800

    fix bug

--- my-app/src/components/st/RemindingText.js
+++ my-app/src/components/st/RemindingText.js
@@ -4,7 +4,7 @@ export default function RemindingText() {
   return (
     <>
       <div className="col-12 col-md-10 remindingarea">
-        <p className="remindingtext">您尚未預訂任何課程 </p>
+        <p className="remindingtext">您尚未購買任何課程 </p>
         <button className="btn  row buy-btn">
           <Link to="/Course/CsCourse">
             <span>購買課程</span>{' '}
